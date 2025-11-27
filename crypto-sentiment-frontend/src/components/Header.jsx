e56c0ce7529e2b7d2d7196c8358cde02f8091696
Ariba Faryad <153382707+aribaf@users.noreply.github.com>
2025-11-27 18:25:18 +0500

    Change logo image filename

--- crypto-sentiment-frontend/src/components/Header.jsx
+++ crypto-sentiment-frontend/src/components/Header.jsx
@@ -1,7 +1,7 @@
 // src/components/Header.jsx
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Logo from "../assets/white.png";
+import Logo from "../assets/WHITE.png";
 
 export default function Header() {
   const [open, setOpen] = useState(false);

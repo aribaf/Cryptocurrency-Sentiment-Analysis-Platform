4a0b8f59aa7388685ab58116864b2a52ccea29fd
Ariba Faryad <153382707+aribaf@users.noreply.github.com>
2026-01-08 18:39:15 +0500

    Change SessionMiddleware same_site attribute to 'none'

--- api/main.py
+++ api/main.py
@@ -28,7 +28,7 @@ app.add_middleware(
 app.add_middleware(
     SessionMiddleware,
     secret_key=os.getenv("SECRET_KEY"),
-    same_site="lax",
+    same_site="none",
     https_only=False
 )
 # ❌ REMOVE SessionMiddleware (do NOT use it now)
